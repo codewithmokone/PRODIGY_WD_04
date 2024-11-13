ffcdd1e40bd273dc4ce39351df6862384c3ea524
codewithmokone <codewithmokone@gmail.com>
2024-11-13 22:01:59 +0200

    added about and contact pages

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello JS.");
+const mobileMenuOpen = document.querySelector('.ham-burger-icon');
+const mobileMenuClose = document.querySelector('.close-icon')
+const mobileLinks = document.querySelector('.mobile-nav');
+
+
+const showMobileMenu = () => {
+    mobileLinks.classList.remove('hidden');
+    mobileMenuClose.classList.remove('hidden');
+    mobileMenuOpen.classList.add('hidden');
+}
+
+const closeMobileMenu = () => {
+    mobileLinks.classList.add('hidden');
+    mobileMenuClose.classList.add('hidden');
+    mobileMenuOpen.classList.remove('hidden');
+}
+
+mobileMenuOpen.addEventListener('click',showMobileMenu);
+mobileMenuClose.addEventListener('click',closeMobileMenu);
