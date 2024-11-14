c777835453170bf56a78fd39c07e71f7676d12d7
codewithmokone <codewithmokone@gmail.com>
2024-11-14 10:11:39 +0200

    added contact section and updated navbar ui

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-const mobileMenuOpen = document.querySelector('.ham-burger-icon');
-const mobileMenuClose = document.querySelector('.close-icon')
-const mobileLinks = document.querySelector('.mobile-nav');
+const mobileMenuOpen = document.querySelector(".ham-burger-icon");
+const mobileMenuClose = document.querySelector(".close-icon");
+const mobileLinks = document.querySelector(".mobile-nav");
 
+// Get all buttons with class="btn" inside the container
+const navMenu = document.querySelector(".nav-menu");
+const links = navMenu.querySelectorAll(".nav-link");
 
+// Loop through the buttons and add the active class to the current/clicked button
+links.forEach((link) => {
+  link.addEventListener("click", function () {
+    // Remove the "active" class from all links
+    links.forEach((item) => item.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+// Function for opening mobile menu
 const showMobileMenu = () => {
-    mobileLinks.classList.remove('hidden');
-    mobileMenuClose.classList.remove('hidden');
-    mobileMenuOpen.classList.add('hidden');
-}
+  mobileLinks.classList.remove("hidden");
+  mobileMenuClose.classList.remove("hidden");
+  mobileMenuOpen.classList.add("hidden");
+};
 
+// Function for closing mobile menu
 const closeMobileMenu = () => {
-    mobileLinks.classList.add('hidden');
-    mobileMenuClose.classList.add('hidden');
-    mobileMenuOpen.classList.remove('hidden');
-}
+  mobileLinks.classList.add("hidden");
+  mobileMenuClose.classList.add("hidden");
+  mobileMenuOpen.classList.remove("hidden");
+};
 
-mobileMenuOpen.addEventListener('click',showMobileMenu);
-mobileMenuClose.addEventListener('click',closeMobileMenu);
+mobileMenuOpen.addEventListener("click", showMobileMenu);
+mobileMenuClose.addEventListener("click", closeMobileMenu);
